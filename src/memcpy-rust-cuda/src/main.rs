55e06ac3e80fb53944feb8910ede4f667c9d07b1
Eiffert B C <35e@equinox.ftpn.ornl.gov>
2024-09-05 15:49:35 -0400

    memcpy-rust-cuda improvements

--- src/memcpy-rust-cuda/src/main.rs
+++ src/memcpy-rust-cuda/src/main.rs
@@ -2,19 +2,27 @@ use cudarc::driver::{CudaDevice, CudaSlice, DriverError};
 use std::time::Instant;
 
 fn main() -> Result<(), DriverError> {
-    let arr: [usize; 16] = [8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144];
+    let vec = vec![8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144];
     let dev = CudaDevice::new(0)?;
     
-    let warmup: CudaSlice<f64> = dev.alloc_zeros::<f64>(1)?;
-    let warmup_host: Vec<f64> = dev.sync_reclaim(warmup)?;
+    // warmup GPU
+    let mut warmup = dev.alloc_zeros::<f64>(1)?;
+    dev.htod_copy_into(vec![1.0; 1], &mut warmup)?;
+    dev.sync_reclaim(warmup)?;
 
-    for i in arr {        
+    for i in vec {
+        
+        // host to device
+        let to_be_copied = vec![1.0; i];
+        let mut a = dev.alloc_zeros::<f64>(i)?;
         let mut now = Instant::now();
-        let a: CudaSlice<f64> = dev.alloc_zeros::<f64>(i)?;
+        dev.htod_copy_into(to_be_copied, &mut a)?;
         let time_to_device = now.elapsed();
-
+        
+        // device to host
         now = Instant::now();
         let a_host: Vec<f64> = dev.sync_reclaim(a)?;
+        //let a_host = dev.dtoh_sync_copy(&a)?; // slow
         let time_to_host = now.elapsed();
         
         println!("Copy {:?} bytes from host to device: {:?} us", i*size_of::<usize>(), time_to_device.subsec_nanos() as f32 / 1000 as f32);

5ea351e6006038e796faa274696e5516645263a7
Brett <brettceiffert@gmail.com>
2024-10-07 17:47:53 -0400

    fix timing output

--- src/memcpy-rust-cuda/src/main.rs
+++ src/memcpy-rust-cuda/src/main.rs
@@ -2,7 +2,8 @@ use cudarc::driver::{CudaDevice, DriverError};
 use std::time::Instant;
 
 fn main() -> Result<(), DriverError> {
-    let vec = vec![8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144];
+    //let vec = vec![8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144];
+    let vec = vec![262144, 524288, 1048576, 2097152, 2097152, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912];
     let dev = CudaDevice::new(0)?;
     
     // warmup GPU
@@ -27,7 +28,7 @@ fn main() -> Result<(), DriverError> {
         
         println!("Copy {:?} bytes from host to device: {:?} us", i*size_of::<usize>(), host_to_device.subsec_nanos() as f32 / 1000 as f32);
         println!("Copy {:?} bytes from device to host: {:?} us", i*size_of::<usize>(), device_to_host.subsec_nanos() as f32 / 1000 as f32);
-        println!("Timing gap in nanoseconds per byte: {:?} us\n", (host_to_device.subsec_nanos() as f32 - device_to_host.subsec_nanos() as f32).abs() / (i*size_of::<usize>()) as f32);
+        println!("Timing gap in nanoseconds per byte: {:?} ns\n", (host_to_device.subsec_nanos() as f32 - device_to_host.subsec_nanos() as f32).abs() / (i*size_of::<usize>()) as f32);
 
         assert_eq!(a_host.len(), i);
 

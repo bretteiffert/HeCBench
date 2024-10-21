a68a30d65c49dc841e7162f074a1fa7588c67d8e
Brett <brettceiffert@gmail.com>
2024-10-21 15:35:14 -0400

    single stream

--- src/triad-rust-cuda/src/main.rs
+++ src/triad-rust-cuda/src/main.rs
@@ -1,45 +1,56 @@
 use cudarc::driver::{CudaDevice, DriverError, LaunchAsync, LaunchConfig};
 use cudarc::nvrtc::compile_ptx;
+use rand::{distributions::Uniform, Rng};
 
 const PTX_SRC: &str = "
-extern \"C\" __global__ void triad(float* a, float* b, float* c, float s) {
+extern \"C\" __global__ void triad(float* a, float* b, float* c, float s, float length) {
     int id = blockDim.x * blockIdx.x + threadIdx.x;
-    if(id < 4) c[id] = a[id] + s*b[id];
+    if(id < length) c[id] = a[id] + s*b[id];
 }
 
 ";
 
 fn main() -> Result<(), DriverError> {
-    let start = std::time::Instant::now();
-
+    
+    let vec = vec![2];
+    
     let ptx = compile_ptx(PTX_SRC).unwrap();
-    println!("Compilation succeeded in {:?}", start.elapsed());
 
     let dev = CudaDevice::new(0)?;
-    println!("Built in {:?}", start.elapsed());
 
     dev.load_ptx(ptx, "triad", &["triad"])?;
     let f = dev.get_func("triad", "triad").unwrap();
-    println!("Loaded in {:?}", start.elapsed());
 
-    let a_host = [1.0f32, 2.0, 3.0, 4.0];
-    let b_host = [1.0f32, 2.0, 3.0, 4.0];
-    let mut c_host = [0.0f32; 4];
+    for i in vec {
 
-    let a_dev = dev.htod_sync_copy(&a_host)?;
-    let b_dev = dev.htod_sync_copy(&b_host)?;
-    let mut c_dev = dev.htod_sync_copy(&c_host)?;
+        let mut a_host = vec![0f32; i];
+        rand::thread_rng().fill(&mut a_host[..]); 
+        let mut b_host = vec![0f32; i];
+        rand::thread_rng().fill(&mut b_host[..]);
+        
+        println!("A {:?}", a_host);
+        println!("B {:?}", b_host);
 
-    println!("Copied in {:?}", start.elapsed());
+        let mut c_host = vec![0.0f32; i];
 
-    let cfg = LaunchConfig {
-        block_dim: (4,1,1),
-        grid_dim: (1,1,1),
-        shared_mem_bytes: 0,
-    };
-    unsafe { f.launch(cfg, (&a_dev, &b_dev, &mut c_dev, 10 as f32)) }?;
+        let a_dev = dev.htod_sync_copy(&a_host)?;
+        let b_dev = dev.htod_sync_copy(&b_host)?;
+        let mut c_dev = dev.htod_sync_copy(&c_host)?;
 
-    dev.dtoh_sync_copy_into(&c_dev, &mut c_host)?;
-    println!("Found {:?} in {:?}", c_host, start.elapsed());
+        let stream = dev.fork_default_stream()?;
+        //println!("stream {:?}", stream);
+        let cfg = LaunchConfig {
+            block_dim: (i as u32,1,1),
+            grid_dim: (1,1,1),
+            shared_mem_bytes: 0,
+        };
+        unsafe { f.clone().launch_on_stream(&stream, cfg, (&a_dev, &b_dev, &mut c_dev, 10 as f32, i as f32)) }?;
+
+        dev.wait_for(&stream)?;
+
+        dev.dtoh_sync_copy_into(&c_dev, &mut c_host)?;
+        println!("Found {:?}", c_host);
+
+    }
     Ok(())
 }

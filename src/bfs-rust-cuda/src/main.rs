538c1a836810054bdedbead26b06201534eac95b
Brett <brettceiffert@gmail.com>
2024-10-27 20:50:39 -0400

    set up file reading and mallocs

--- src/bfs-rust-cuda/src/main.rs
+++ src/bfs-rust-cuda/src/main.rs
@@ -1,35 +1,96 @@
-use cudarc::driver::{CudaDevice, DriverError};
+use cudarc::{
+    driver::{CudaDevice, DriverError, LaunchAsync, LaunchConfig},
+    nvrtc::Ptx,
+};
+
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
+
+
 use std::time::Instant;
 
+const MAX_THREADS_PER_BLOCK: usize = 256;
 
-def bfs_cpu(adj, s) {
+struct Node {
+  starting: usize,
+  no_of_edges: usize,
+}
+
+fn lines_from_file(filename: impl AsRef<Path>) -> Vec<String> {
+    let file = File::open(filename).expect("no such file");
+    let buf = BufReader::new(file);
+    buf.lines()
+        .map(|l| l.expect("Could not parse line"))
+        .collect()
+}
+
+fn main() -> Result<(), DriverError> {
     
 
-}
+    let lines = lines_from_file("graph1MW_6.txt");
+    let mut source: usize = 0;
 
-fn add_edge(adj: &mut Vec<Vec<usize>>, u: usize, v: usize) {
-    adj[u].push(v);
-    adj[v].push(u);
-}
+    //println!("{:?}", lines[0]);
+    let no_of_nodes: usize = lines[0].parse::<usize>().unwrap();
+    println!("{:?}", no_of_nodes);
+    
+    let mut h_graph_nodes: Vec<Node> = Vec::new();
+    let mut h_graph_mask: Vec<usize> = vec![0; no_of_nodes];
+    let mut h_updating_graph_mask: Vec<usize> = vec![0; no_of_nodes];
+    let mut h_graph_visited: Vec<usize> = vec![0; no_of_nodes];
 
-
-fn main() {
-    let vert = 7;
-    let mut adj: Vec<Vec<usize>> = vec![vec![]];
-    for i in 1..vert {
-        adj.push(vec![])
+    for i in 1..(no_of_nodes + 1) {
+        let parse_line = lines[i].split(" ").collect::<Vec<_>>();
+        let node = Node {
+            starting: parse_line[0].parse::<usize>().unwrap(),
+            no_of_edges: parse_line[1].parse::<usize>().unwrap(),
+        };
+        //println!("{:?}", i);
+        h_graph_nodes.push(node);
     }
-    add_edge(&mut adj, 0, 1);
-    add_edge(&mut adj, 0, 2);
-    add_edge(&mut adj, 1, 2);
-    add_edge(&mut adj, 2, 3);
-    add_edge(&mut adj, 2, 4);
-    add_edge(&mut adj, 3, 6);
-    add_edge(&mut adj, 4, 6);
-    add_edge(&mut adj, 6, 5);
+    
+    source = lines[no_of_nodes + 1].parse::<usize>().unwrap();
+    source = 0;
+    //println!("{:?}", lines[no_of_nodes + 1]);
+    
+    h_graph_mask[source]=1;
+    h_graph_visited[source]=1;
 
-    println!("{:?}\n", adj);
+    let edge_list_size: usize = lines[no_of_nodes + 1 + 1].parse::<usize>().unwrap();
+    
+    //println!("{:?}", lines[no_of_nodes + 1 + 1 + 1]);
+    
+    let mut h_graph_edges: Vec<usize> = Vec::new();
+    let mut edge_list = lines[no_of_nodes + 1 + 1 + 1].split(" ").collect::<Vec<_>>();
+    for i in (0..edge_list_size*2).step_by(2) {
+        let id: usize= edge_list[i].parse::<usize>().unwrap();
+        h_graph_edges.push(id);
+    }
+    
+    //println!("{:?}", h_graph_edges[h_graph_edges.len() - 1]);
+
+    let mut h_cost: Vec<usize> = vec![1; no_of_nodes];
+    let mut h_cost_ref: Vec<isize> = vec![-1; no_of_nodes];
+
+
+
+    /*
+    // declare device
+    let dev = CudaDevice::new(0)?;
+    
+    // load code onto gpu
+    dev.load_ptx(Ptx::from_file("/home/35e/HeCBench/src/bfs-rust-cuda/src/bfs-kernel.ptx"), "BFS", &["BFS_Kernel"])?;
+    dev.load_ptx(Ptx::from_file("/home/35e/HeCBench/src/bfs-rust-cuda/src/bfs-kernel.ptx"), "BFS2", &["BFS_Kernel2"])?;
+    let f = dev.get_func("BFS", "BFS_Kernel").unwrap();
+    let f2 = dev.get_func("BFS2", "BFS_Kernel2").unwrap();
+    */
+
     
 
 
+
+    println!("hello world");
+    
+    Ok(())
 }
